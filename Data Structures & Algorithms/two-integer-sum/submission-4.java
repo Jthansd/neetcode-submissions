d75428cf85edf3e8b154d65e6f0005d1274492ac
Jonathan Wilson <jonathanwilsonsd@gmail.com>
2026-09-24 14:20:31 -0700

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int left = 0, right = 1;
+
+        while(nums[left] + nums[right] != target){
+            if(right < nums.length - 1){
+                right++;
+            }
+            else{
+                left++;
+                right = left + 1;
+            }
+        }
+        
+        return new int[] {left, right};
+
+    }
+}
